10b04a3dc08c5431fbb9554c2f0f56e1e81cf99f
BrunoMBAlmeida <92309156+BrunoMBAlmeida@users.noreply.github.com>
2021-10-11 15:37:55 +0100

    Add files via upload

--- ArmazemGestão/ArmazemGestao-main/v1/armazem.js
+++ ArmazemGestão/ArmazemGestao-main/v1/armazem.js
@@ -1,8 +1,37 @@
 const nome = "Armazém"; // declara uma constante e atribui o valor "Armazém" (um 'string', uma cadeia de carateres)
 
 // Definir tipos (classes) de objetos que a app gerirá ("type of objects")
-class Item {}
+class Item {
+    #quantidade;
+    constructor(codigo, nome){
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+    toString() {
+        return `${this.itemCompleto} (º ${this.#quantidade})`;
+    }
+    get itemCompleto () {
+        return `${this.codigo} ${tjis.nome}`;
+    }
+    set quantidade(valor){
+        this.#quantidade = valor;
+    }
+}
 
-class Espaço {}
+class Espaço {
+    constructor(corredor, secção, andar){
+        this.corredor = corredor;
+        this.secção = secção;
+        this.andar = andar
+    }
+    
+}
 
-function Localização() {} // Sintax alternativa (anterior a ES6) para definir classes de objetos
+function Localização() {
+    var item = item;
+    var Espaço = Espaço
+} // Sintax alternativa (anterior a ES6) para definir classes de objetos
+
+Localização.prototype.imprimir = function() {
+    return `O item ${this.item} encontra no espaço ${this.Espaço}`;
+}
