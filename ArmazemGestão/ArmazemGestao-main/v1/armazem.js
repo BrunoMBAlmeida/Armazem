62f9e5177e44de475796a50b7440e2a6938d5658
BrunoMBAlmeida <92309156+BrunoMBAlmeida@users.noreply.github.com>
2021-10-11 12:40:56 +0100

    Add files via upload

---
+++ ArmazemGestão/ArmazemGestao-main/v1/armazem.js
@@ -0,0 +1,8 @@
+const nome = "Armazém"; // declara uma constante e atribui o valor "Armazém" (um 'string', uma cadeia de carateres)
+
+// Definir tipos (classes) de objetos que a app gerirá ("type of objects")
+class Item {}
+
+class Espaço {}
+
+function Localização() {} // Sintax alternativa (anterior a ES6) para definir classes de objetos
